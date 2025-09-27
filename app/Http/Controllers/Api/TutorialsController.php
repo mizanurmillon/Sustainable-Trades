67d42e581aaf76ddf3aab998b745a6657321d5e7
mizanurmillon <mr7517218@gmail.com>
2025-09-27 11:40:09 +0600

    Tutorial search by name

--- app/Http/Controllers/Api/TutorialsController.php
+++ app/Http/Controllers/Api/TutorialsController.php
@@ -17,16 +17,22 @@ class TutorialsController extends Controller
     {
         $cms = Cms::where('page_name', Page::TUTORIALS->value)->select(['id','image'])->first();
 
-        $query = Tutorial::where('status', 'active')->latest()->get();
+        $query = Tutorial::where('status', 'active');
+
+        if ($request->has('search')) {
+            $query->where('name', 'like', '%' . $request->search . '%');
+        }
 
         if($request->has('type'))
         {
             $query = $query->where('type', $request->type);
         }
 
+        $tutorials = $query->latest()->get();
+
         $data = [
             'banner' => $cms,
-            'tutorials' => $query
+            'tutorials' => $tutorials,
         ];
 
         return $this->success($data, 'Tutorials fetched successfully.', 200);
