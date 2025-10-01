343cd1c455d504153e92d8253cd9407b519b0ae5
mizanurmillon <mr7517218@gmail.com>
2025-10-01 10:56:13 +0600

    product quantity and unlimited stoc out of stock selling option pass it

--- app/Http/Controllers/Api/Vendor/ProductController.php
+++ app/Http/Controllers/Api/Vendor/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
         $validator = Validator::make($request->all(), [
             'product_name' => 'required|string|max:255',
             'product_price' => 'required|numeric|min:0',
-            'product_quantity' => 'required|numeric|min:1',
+            'product_quantity' => 'nullable|numeric|min:1',
             'weight'=> 'nullable|numeric|min:0',
             'cost' => 'nullable|numeric|min:0',
             'unlimited_stock' => 'nullable|boolean',
