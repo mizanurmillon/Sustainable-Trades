6957ee3887290e48728664ce9c14eb7427b45c76
mizanurmillon <mr7517218@gmail.com>
2025-10-07 09:04:55 +0600

    issue fixed

---
+++ app/Http/Controllers/Api/TopVendorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+
+class TopVendorController extends Controller
+{
+    use ApiResponse;
+    
+    public function topVendors()
+    {
+        $query = User::with('shopInfo:id,user_id,shop_name,shop_image,shop_banner', 'shopInfo.address')
+            ->where('role', 'vendor')
+            ->where('status', 'active')
+            ->whereHas('shopInfo')
+            ->select('id', 'first_name', 'last_name', 'role', 'avatar');
+
+        $data = $query->get();
+
+        if ($data->isEmpty()) {
+            return $this->error([], 'No shops found', 404);
+        }
+
+        return $this->success($data, 'All top shops retrieved successfully', 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,32 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\LoginController;
-use App\Http\Controllers\Api\Auth\RegisterController;
-use App\Http\Controllers\Api\Auth\ShopOwnerController;
-use App\Http\Controllers\Api\BannerController;
-use App\Http\Controllers\Api\Cart\CartController;
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\ContactController;
-use App\Http\Controllers\Api\DynamicPageController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FaqController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\Cart\CartController;
+use App\Http\Controllers\Api\Shop\ShopController;
+use App\Http\Controllers\Api\TopVendorController;
+use App\Http\Controllers\Api\TutorialsController;
+use App\Http\Controllers\Api\AllProductController;
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\FollowShopController;
-use App\Http\Controllers\Api\GetNotificationController;
 use App\Http\Controllers\Api\MyFavoriteController;
 use App\Http\Controllers\Api\NewsletterController;
 use App\Http\Controllers\Api\OurMissionController;
-use App\Http\Controllers\Api\Product\ProductController;
-use App\Http\Controllers\Api\AllProductController;
-use App\Http\Controllers\Api\Shop\ShopController;
-use App\Http\Controllers\Api\SitesettingController;
 use App\Http\Controllers\Api\SocialAuthController;
 use App\Http\Controllers\Api\SocialLinkController;
-use App\Http\Controllers\Api\SubscriptionPlanController;
-use App\Http\Controllers\Api\TutorialsController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DynamicPageController;
+use App\Http\Controllers\Api\SitesettingController;
 use App\Http\Controllers\Api\UserSettingController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\ShopOwnerController;
+use App\Http\Controllers\Api\GetNotificationController;
+use App\Http\Controllers\Api\Product\ProductController;
+use App\Http\Controllers\Api\SubscriptionPlanController;
 use App\Http\Controllers\Api\Vendor\SpotlightApplicationController;
-use Illuminate\Support\Facades\Route;
 
 
 
@@ -172,6 +173,10 @@ Route::group(['middleware' => ['guest']], function () {
     Route::controller(AllProductController::class)->group(function () {
         Route::get('/all-products', 'allProducts');
         Route::get('/is-featured-product', 'isFeaturedProduct');
+    });
+
+    Route::controller(TopVendorController::class)->group(function () {
+        Route::get('/top-vendors', 'topVendors');
     });
 });
 
