019e1e509ae1ab301fa6ea315ea21a0b9a820a4e
mizanurmillon <mr7517218@gmail.com>
2026-01-01 15:29:30 +0600

    admin dashboard full completed

--- app/Http/Controllers/Web/Backend/DashboardController.php
+++ app/Http/Controllers/Web/Backend/DashboardController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Web\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Membership;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('backend.layouts.index');
+    public function index()
+    {
+
+        $totalProMembers = Membership::where('membership_type', 'pro')->count();
+        $totalBasicMembers = Membership::where('membership_type', 'basic')->count();
+        $totalCustom = User::where('role', 'customer')->count();
+
+        $totalListingRequests = Product::where('status', 'pending')->count();
+        $totalActiveListings = Product::where('status', 'approved')->count();
+
+        $totalRevenue = Membership::sum('price');
+
+        return view('backend.layouts.index', compact('totalProMembers', 'totalBasicMembers', 'totalCustom', 'totalListingRequests', 'totalActiveListings', 'totalRevenue'));
     }
 }
